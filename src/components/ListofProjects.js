b5eb030b12d55335e9df3bff40ff57b373b0b347
github-aliceyang <aliyang47@gmail.com>
2023-05-23 14:05:22 -0700

    created the projects list object file

---
+++ src/components/ListofProjects.js
@@ -0,0 +1,50 @@
+export const ProjectList = [
+    {
+        title: 'Weather Application',
+        img: require('../images/weather.png'),
+        des: `This application was coded in an advanced 8-week coding workshop. Building upon my previously-gained knowledge, I also became skilled in Bootstrap, API’s, real-life development workflow, hosting, and advanced JavaScript. I combined all of these skills to create a fully-functioning weather app.`,
+        demo: `https://sensational-moonbeam-d55027.netlify.app/`,
+        github: `https://github.com/Ayang471/react-weather-app`,
+    },
+    {
+        title: 'Dictionary Application',
+        img: require('../images/dictionary.png'),
+        des: `I immersed myself in React development in SheCodes React. I used my knowledge of this highly-demanded framework to build a working dictionary app incorporating coding best practices, along with the most popular tools and technologies.`,
+        demo: `https://creative-tiramisu-c25be9.netlify.app`,
+        github: `https://github.com/Ayang471/homework-dictionary-project`,
+    },
+    {
+        title: 'Little Lemon Website',
+        img: require('../images/HomePage.png'),
+        des: `I developed a responsive web app for table reservations at the Little Lemon restaurant in the Front-End Developer Capstone course. Applying my front-end skills, I coded a functional website and utilized React frameworks like Material UI and Chakra components for improved responsiveness and an engaging interface. The outcome is a visually appealing and dynamic user experience.`,
+        demo: `https://resplendent-sawine-9cbd46.netlify.app`,
+        github: `https://github.com/Ayang471/little-lemon-website`,
+    }
+]
+
+export const DesignWorks = [
+    {
+        title: 'Discover - The Apartment Finder ',
+        img: require('../images/discover.png'),
+        des: `The primary objective of the workshop from DesignLab was to familiarize myself with prevalent design patterns and incorporate best practices for creating UI components. I also honed my visual design skills in areas such as color, typography, and iconography. I acquired the ability to identify and replicate essential design patterns, culminating in the creation of a high fidelity prototype that showcases my newly acquired knowledge.`,
+        demo: `https://fancy-bonbon-814687.netlify.app/`,
+    },
+    {
+        title: 'Healthy Bites',
+        img: require('../images/HealthyBites.png'),
+        des: `I utilized Adobe XD to successfully create the Healthy Bites mobile app, incorporating components for an efficient design process. This included building an interactive prototype and designing the full mobile app mockup, as well as creating a visually appealing landing page.`,
+        demo: `https://cute-flan-27f90d.netlify.app/`,
+    },
+    {
+        title: 'Landing Page and Mobile App',
+        img: require(`../images/Plant.png`),
+        des: `Utilizing the skills I acquired from the workshops, I applied them to design this landing page and mobile app pages using Figma.`,
+        demo: `https://www.figma.com/proto/DusinD6wnSbHCPvtyTrclv/Landing-page-ideas?page-id=408%3A554&type=design&node-id=408-709&viewport=1098%2C496%2C0.53&scaling=min-zoom`,
+    },
+    {
+        title: 'Landing Page Challenge',
+        img: require(`../images/hometown.png`),
+        des: `Utilizing the skills I acquired from the workshops and applied them to design this landing page.`,
+        demo: `https://www.figma.com/proto/DusinD6wnSbHCPvtyTrclv/Landing-page-ideas?page-id=12%3A222&type=design&node-id=12-221&viewport=5656%2C803%2C0.29&scaling=min-zoom`,
+    },
+]
