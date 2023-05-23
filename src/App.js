b17c44f65ee686cb5da0346693e46a9e6efb19e3
github-aliceyang <aliyang47@gmail.com>
2023-05-23 14:07:33 -0700

    updated nav bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import './App.css';
+//import Cards from './components/Cards'
+import HeroSection from './components/HeroSection';
+import ProjectsSection from './components/ProjectsSection';
+import SkillsSection from './components/SkillsSection';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-     hello
-      </header>
+      <div className='nav-bar'>
+        <ul className="nav justify-content-center">
+          <li className="nav-item">
+            <a className="nav-link" href="/">Home</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/">Projects</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/">Skills</a>
+          </li>
+        </ul>
+      </div>
+      <ProjectsSection />
+      <SkillsSection />
     </div>
   );
 }
