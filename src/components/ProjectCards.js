13ddc530f224c7bdb4b63b8070a15e52c930ac34
github-aliceyang <aliyang47@gmail.com>
2023-05-24 12:17:42 -0700

    updated the file

--- src/components/ProjectCards.js
+++ src/components/ProjectCards.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '../stylesheets/projectCards.css'
 import { ProjectList, DesignWorks } from './ListofProjects'
+import { Show, Hide } from '@chakra-ui/react'
 
 function ProjectCards() {
 
@@ -9,19 +10,21 @@ function ProjectCards() {
             <div className='container'>
                 <div className="row ">
                     {ProjectList.map((list, index) => (
-                        <div className="col" key={`project-${index}`}>
-                            <div>
+                        <div className="col" key={index}>
+                           
                                 <img src={list.img} alt="weather-app" />
-                            </div>
-                            <div className='border'></div>
-                      
+                          
+                            <Show breakpoint='(min-width: 600px)' >
+                                <div className='border'></div>
+                            </Show>
+                           
                                 <div className="card-body">
                                     <h5 className="card-title">{list.title}</h5>
                                     <p className="card-text">{list.des}</p>
-                                    <div className='btn'>
-                                        <a href={list.demo} target="_blank" rel="noopener noreferrer">Demo</a>
-                                        <a href={list.github} target="_blank" rel="noopener noreferrer">GitHub</a>
-                                    </div>
+                                  
+                                        <a href={list.demo}  target="_blank" rel="noopener noreferrer">Demo</a>
+                                        <a href={list.github}  target="_blank" rel="noopener noreferrer">GitHub</a>
+                                    
                                 </div>
                         </div>
                     ))}
@@ -29,23 +32,25 @@ function ProjectCards() {
                 <div className='design-works'>
                     <div className="row ">
                         {DesignWorks.map((list, index) => (
-                            <div className="col" key={`design-${index}`}>
-                                <div>
+                            <div className="col" key={index}>
+
+                                <div className='image'>
                                     <img src={list.img} alt="weather-app" />
                                 </div>
+
                                 <div className='border'></div>
                               
                                     <div className="card-body">
                                         <h5 className="card-title">{list.title}</h5>
                                         <p className="card-text">{list.des}</p>
-                                        <div className='btn'>
                                             <a href={list.demo} target="_blank" rel="noopener noreferrer">Demo</a>
-                                        </div>
                                     </div>
                                 </div>
                           
                         ))}
                     </div>
+                </div>
+                <div>                
                 </div>
             </div>
         </div>
