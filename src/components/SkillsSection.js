c33f9771be0b86dd21b837f6d55692e49d5c813a
github-aliceyang <aliyang47@gmail.com>
2023-05-24 12:18:23 -0700

    edited the skills section

--- src/components/SkillsSection.js
+++ src/components/SkillsSection.js
@@ -1,10 +1,28 @@
 import React from 'react'
+import '../stylesheets/SkillsSectionStyles.css'
+import Skills from '../components/Skills'
 
 function SkillsSection() {
   return (
     <div className='skills-section'>
-      SkillsSection
+      <div className='container'>
+        <div className='row'>
+        
+            <div className='skills-intro'>
+              <div className='border'> </div>
+              <h1>
+                Technical
+                <br />
+                Skills
+              </h1>
+            </div>
+          </div>
+     
+        <section>
+          <Skills />
+        </section>
       </div>
+    </div>
   )
 }
 
