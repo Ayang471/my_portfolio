6b9aaf692d3aed9fd5ab6175c9b5a67ed7516059
github-aliceyang <aliyang47@gmail.com>
2023-05-24 13:30:10 -0700

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import ProjectsSection from './components/ProjectsSection';
 import SkillsSection from './components/SkillsSection';
-import MyResume from './components/MyResume'
 import { Routes, Route, Link } from 'react-router-dom'
 
 function App() {
@@ -11,14 +10,13 @@ function App() {
         <ul className="nav justify-content-center">
           <Link to="/projects" className="nav-link">Projects</Link>
           <Link to="/skills" className="nav-link">Skills</Link>
-          <Link to="/resume" className="nav-link">Resume</Link>
+          <a href="https://drive.google.com/file/d/1WMyE2gunzDJ38F2p3nvA-sAHed-3VC33/view?usp=sharing" target="_blank" rel="noopener noreferrer" className="nav-link">Resume</a>
         </ul>
       </nav>
-<Routes>
-  <Route path="/projects" element={<ProjectsSection />} />
-        <Route path="/skills" element={<SkillsSection/>} />
-        <Route path="/resume" element={<MyResume />} />
-</Routes>
+      <Routes>
+        <Route path="/projects" element={<ProjectsSection />} />
+        <Route path="/skills" element={<SkillsSection />} />
+      </Routes>
     </div>
   );
 }
