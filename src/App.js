6de7cdf1e1ccb53e70ff9a01bb58303b2fad2356
github-aliceyang <aliyang47@gmail.com>
2023-05-24 15:45:26 -0700

    added home route and link

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import ProjectsSection from './components/ProjectsSection';
 import SkillsSection from './components/SkillsSection';
-
+import Homepage from './components/Homepage';
 import { Routes, Route, Link } from 'react-router-dom'
 
 function App() {
@@ -9,14 +9,14 @@ function App() {
     <div className="App">
       <nav className='nav-bar'>
         <ul className="nav justify-content-center">
-
+          <Link to="/home" className="nav-link">Home</Link>
           <Link to="/projects" className="nav-link">Projects</Link>
           <Link to="/skills" className="nav-link">Skills</Link>
           <a href="https://drive.google.com/file/d/1WMyE2gunzDJ38F2p3nvA-sAHed-3VC33/view?usp=sharing" target="_blank" rel="noopener noreferrer" className="nav-link">Resume</a>
         </ul>
       </nav>
       <Routes>
-  
+        <Route path="/home" element={<Homepage />} />
         <Route path="/projects" element={<ProjectsSection />} />
         <Route path="/skills" element={<SkillsSection />} />
       </Routes>
