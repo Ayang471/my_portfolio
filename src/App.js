b326cde0e8a11006caea771ceb39d9723cda94aa
github-aliceyang <aliyang47@gmail.com>
2023-05-25 16:55:59 -0700

    removed the overlay codes

--- src/App.js
+++ src/App.js
@@ -1,43 +1,16 @@
 import './App.css';
-import React, { useState } from 'react';
+import React from 'react';
 import ProjectsSection from './components/ProjectsSection';
 import SkillsSection from './components/SkillsSection';
 import Homepage from './components/Homepage';
-import { Routes, Route, Link, useNavigate } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
+import OverlayNav from './components/OverlayNav';
 
 function App() {
-  const [isActive, setIsActive] = useState(false);
-
-  const handleClick = (e) => {
-    e.preventDefault();
-    setIsActive(!isActive);
-  };
-
-
 
   return (
     <div className="App">
-      <a href="/" title="" className={`btn ${isActive ? 'is-active' : ''}`} onClick={handleClick}>
-        <span></span>
-        <span></span>
-      </a>
-      <div className={`menu ${isActive ? 'is-active' : ''}`} onClick={handleClick}>
-        <ul className="menu_list justify-content-center">
-         <Link to="/home" title="" className='menu-link'>Home</Link>
-          <Link to="/projects" title="" className='menu-link'>Projects</Link>
-          <Link to="/skills" title="" className='menu-link'>Skills</Link>
-          <Link href="https://drive.google.com/file/d/1WMyE2gunzDJ38F2p3nvA-sAHed-3VC33/view?usp=sharing" title="" target="_blank" rel="noopener noreferrer" className="menu-link">Resume</Link>
-        </ul>
-</div>
-     {/* <nav className='nav-bar'>
-        <ul className="nav justify-content-center">
-          <Link to="/home" className="nav-link">Home</Link>
-          <Link to="/projects" className="nav-link">Projects</Link>
-          <Link to="/skills" className="nav-link">Skills</Link>
-          <a href="https://drive.google.com/file/d/1WMyE2gunzDJ38F2p3nvA-sAHed-3VC33/view?usp=sharing" target="_blank" rel="noopener noreferrer" className="nav-link">Resume</a>
-        </ul>
-      </nav>
-      */}
+     <OverlayNav />
       <Routes>
         <Route path="/home" element={<Homepage />} />
         <Route path="/projects" element={<ProjectsSection />} />
