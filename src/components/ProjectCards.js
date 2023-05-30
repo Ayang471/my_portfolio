574a0754120476bd150016fb5e394f71fdb19454
github-aliceyang <aliyang47@gmail.com>
2023-05-30 12:46:53 -0700

    modified the cards

--- src/components/ProjectCards.js
+++ src/components/ProjectCards.js
@@ -8,52 +8,59 @@ function ProjectCards() {
     return (
         <div className='project-cards'>
             <div className='container'>
-                <div className="row ">
-                    {ProjectList.map((list, index) => (
-                        <div className="col" key={index}>
-                           
-                                <img src={list.img} alt="weather-app" />
-                          
+                {ProjectList.map((list, index) => (
+                    <div className="row" key={index}>
+                        <div className='col'>
+                            <img src={list.img} alt="weather-app" />
+
                             <Show breakpoint='(min-width: 600px)' >
                                 <div className='border'></div>
-                            
-                           
+                            </Show>
+
+                        </div>
+
+                        <div className='col'>
+                            <div className="card-body">
+                                <h5 className="card-title">{list.title}</h5>
+                                <p className="card-text">{list.des}</p>
+                                <a href={list.demo} target="_blank" rel="noopener noreferrer">Demo</a>
+                                <a href={list.github} target="_blank" rel="noopener noreferrer">GitHub</a>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+                ))}
+
+                <div className='design-works'>
+                    {DesignWorks.map((list, index) => (
+                        <div className="row" key={index}>
+                          
+                                <div className='col'>
+                                    <img src={list.img} alt="weather-app" />
+                             
+
+                            <Show breakpoint='(min-width: 600px)' >
+                                <div className='border'></div>
+                            </Show>
+                            </div>
+
+                            <div className='col'>
                                 <div className="card-body">
                                     <h5 className="card-title">{list.title}</h5>
                                     <p className="card-text">{list.des}</p>
-                                        <a href={list.demo}  target="_blank" rel="noopener noreferrer">Demo</a>
-                                        <a href={list.github}  target="_blank" rel="noopener noreferrer">GitHub</a>
-                                
+                                    <a href={list.demo} target="_blank" rel="noopener noreferrer">Demo</a>
+
                                 </div>
-                        </Show>
+                            </div>
                         </div>
                     ))}
                 </div>
-                <div className='design-works'>
-                    <div className="row ">
-                        {DesignWorks.map((list, index) => (
-                            <div className="col" key={index}>
-
-                                <div className='image'>
-                                    <img src={list.img} alt="weather-app" />
-                                </div>
-
-                                <div className='border'></div>
-                              
-                                    <div className="card-body">
-                                        <h5 className="card-title">{list.title}</h5>
-                                        <p className="card-text">{list.des}</p>
-                                            <a href={list.demo} target="_blank" rel="noopener noreferrer">Demo</a>
-                                    </div>
-                                </div>
-                          
-                        ))}
-                    </div>
-                </div>
-                <div>                
-                </div>
             </div>
+
         </div>
+
 
     )
 }
