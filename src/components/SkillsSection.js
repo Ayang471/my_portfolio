ffdc9c66a6c8fcdc58e792e7d3bf3f621cfde9d2
github-aliceyang <aliyang47@gmail.com>
2023-05-30 12:47:17 -0700

    updated skills section

--- src/components/SkillsSection.js
+++ src/components/SkillsSection.js
@@ -7,6 +7,7 @@ function SkillsSection() {
     <div className='skills-section'>
       <div className='container'>
         <div className='row'>
+          <div className='col'>
             <div className='skills-intro'>
               <div className='border'> </div>
               <h1>
@@ -14,11 +15,11 @@ function SkillsSection() {
               </h1>
             </div>
           </div>
-     
+          </div>
+      </div>
         <section>
           <Skills />
         </section>
-      </div>
     </div>
   )
 }
