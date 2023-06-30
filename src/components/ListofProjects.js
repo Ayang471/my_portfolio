7cd4f31a2ab4d522edbb1ffce13151d050011001
github-aliceyang <aliyang47@gmail.com>
2023-06-30 10:57:11 -0700

    updated the demo link

--- src/components/ListofProjects.js
+++ src/components/ListofProjects.js
@@ -11,7 +11,7 @@ export const ProjectList = [
         title: 'React Dictionary Application',
         img: require('../images/dictionary.png'),
         des: `I immersed myself in the world of React development and applied my extensive knowledge of this highly sought-after framework. The result was the creation of fully functional dictionary, incorporating multiple components and implementing coding best practices. Furthermore, I utilized popular tools and technologies to enhance the quality and efficiency of the applications.`,
-        demo: `https://creative-tiramisu-c25be9.netlify.app`,
+        demo: `https://ay-dictionary.netlify.app/`,
         github: `https://github.com/Ayang471/homework-dictionary-project`,
     },
     {
@@ -20,7 +20,7 @@ export const ProjectList = [
         des: `In the Front-End Developer Capstone course, I developed a web application for table reservations and log-in form at the Little Lemon 
         restaurant. Leveraging my front-end skills and ultilized React frameworks such as Material-UI and Chakra components to enhance its responsiveness and 
         create an engaging interface for a more visual appealing and dynamic user experience.`,
-        demo: `https://resplendent-sawine-9cbd46.netlify.app`,
+        demo: `https://littlelemonwebsite.netlify.app/`,
         github: `https://github.com/Ayang471/little-lemon-website`,
     }
 ]
@@ -32,14 +32,14 @@ export const DesignWorks = [
         des: `The primary objective of the workshop was to familiarize myself with prevalent design patterns and incorporate best practices 
         for creating UI components. I also honed my visual design skills in areas such as color, typography, and iconography. I acquired the ability to 
         identify and replicate essential design patterns, culminating in the creation of a high fidelity prototype that showcases my newly acquired knowledge.`,
-        demo: `https://fancy-bonbon-814687.netlify.app/`,
+        demo: `https://ay-apartment-finder-app.netlify.app/`,
     },
     {
         title: 'Healthy Bites',
         img: require('../images/HealthyBites.png'),
         des: `I utilized Adobe XD to successfully create the Healthy Bites mobile app, incorporating components for an efficient design process. This included 
         building an interactive prototype and designing the full mobile app mockup, as well as creating a visually appealing landing page.`,
-        demo: `https://cute-flan-27f90d.netlify.app/`,
+        demo: `https://ay-healthy-bites.netlify.app//`,
     },
     {
         title: 'Landing Page and Mobile App',
