bf04bd1bef3c3f52e43ce9f9dd21c766ea3c45af
github-aliceyang <aliyang47@gmail.com>
2023-06-30 14:21:49 -0700

    integrated text animation

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import '../stylesheets/heroSectionStyles.css'
 import { Link } from 'react-router-dom'
-
+import { Bounce, Slide } from "react-awesome-reveal";
 
 function HeroSection() {
     return (
@@ -11,26 +11,35 @@ function HeroSection() {
                     <div className='col'>
                         <div className='border'></div>
                         <div className='intro-section'>
-                            <h1>
-                                <span>I</span>
-                                <span>'</span>
-                                <span>m</span>
-                                <br />
-                                <span>A</span>
-                                <span>l</span>
-                                <span>i</span>
-                                <span>c</span>
-                                <span>e</span>
-                                <br />
-
-                            </h1>
+                           <Bounce>
+                                <h1>
+                                    <span>I</span>
+                                    <span>'</span>
+                                    <span>m</span>
+                                    <br />
+                                    <span>A</span>
+                                    <span>l</span>
+                                    <span>i</span>
+                                    <span>c</span>
+                                    <span>e</span>
+                                    <br />
+                                </h1>
+                           </Bounce>
 
                             <div className='intro'>
-                                <h2>Feels free to take a look at some of my works.</h2>
+                                <Slide 
+                                    direction='left'
+                                    >
+                                    <h2>Feels free to take a look at some of my works.</h2>
+                                </Slide>
                             </div>
 
                             <div className='view-btn'>
-                                <Link to="/projects">View Projects</Link>
+                               <Slide 
+                               delay='100'
+                               >
+                                    <Link to="/projects">View Projects</Link>
+                               </Slide>
                             </div>
 
                         </div>
