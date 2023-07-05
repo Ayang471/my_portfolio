a7eb1b8099e20abb976ba3911c759ce2245d0bf4
github-aliceyang <aliyang47@gmail.com>
2023-07-05 16:36:13 -0700

    removed nav

--- src/App.js
+++ src/App.js
@@ -12,22 +12,6 @@ function App() {
 
   return (
     <div className="App" >
-     {/*<nav className="navbar navbar-expand-md fixed-top navbar-shrink justify-content-center align-items-center">
-        <div className="container-fluid">
-          <button className="navbar-toggler mx-3 bg-light" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse justify-content-center align-items-center" id="mynavbar">
-            <div className="navbar-nav justify-content-center align-items-center spacing">
-              <Link to="/" className="nav-link">Home</Link>
-              <Link to="/projects" className="nav-link">Projects</Link>
-              <Link to="/skills" className="nav-link">Skills</Link>
-              <Link href="/" className="nav-link" onClick={openResume}>Resume</Link>
-              <Link href="/" className="nav-link" onClick={openGitHub}>GitHub</Link>
-            </div>
-          </div>
-        </div>
-      </nav>*/}
       <OverlayNav />
         <Routes>
           <Route path="/" element={<Homepage />} />
